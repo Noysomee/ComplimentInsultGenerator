402b69a65208e8d7ae54a6b3410a721a68b77615
SamM <samwm2411@gmail.com>
2023-12-01 14:37:51 +0000

    Added insult generating page and added in a button that switches between the two pages

---
+++ ComplimentInsultGenerator/InsultPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Insult_Generator_Thing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ComplimentInsultGenerator
+{
+    public sealed partial class InsultPage : Page
+    {
+        public InsultPage()
+        {
+            this.InitializeComponent();
+        }
+        private void GenerateInsultButton_Click(object sender, RoutedEventArgs e)
+        {
+            Insults insult = new Insults();
+            Textblock.Text = insult.InsultGenerator();
+        }
+        private void InsultButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
