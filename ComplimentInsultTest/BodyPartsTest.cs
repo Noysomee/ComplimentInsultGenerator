5206dd067654ad8d41a9e4bdeadf3f8613b0a918
SamM <samwm2411@gmail.com>
2023-12-08 11:10:43 +0000

    Added test project for unit testing with BodyPartsTest class

---
+++ ComplimentInsultTest/BodyPartsTest.cs
@@ -0,0 +1,12 @@
+namespace ComplimentInsultTest
+{
+    [TestClass]
+    public class BodyPartsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
